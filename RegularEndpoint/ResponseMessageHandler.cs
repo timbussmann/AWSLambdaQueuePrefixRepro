a4059db244aa2516195a116bc617176e2e5215f1
Tim Bussmann <timbussmann@users.noreply.github.com>
2023-10-05 09:30:46 +0200

    Add project files.

---
+++ RegularEndpoint/ResponseMessageHandler.cs
@@ -0,0 +1,21 @@
+﻿using System.Threading.Tasks;
+using Messages;
+using NServiceBus;
+using NServiceBus.Logging;
+
+public class ResponseMessageHandler : IHandleMessages<ResponseMessage>, IHandleMessages<ReplyMessage>
+{
+    static readonly ILog Log = LogManager.GetLogger<ResponseMessageHandler>();
+
+    public Task Handle(ResponseMessage message, IMessageHandlerContext context)
+    {
+        Log.Info($"Handling {nameof(ResponseMessage)} in RegularEndpoint");
+        return Task.CompletedTask;
+    }
+
+    public Task Handle(ReplyMessage message, IMessageHandlerContext context)
+    {
+        Log.Info($"Handling {nameof(ReplyMessage)} in RegularEndpoint");
+        return Task.CompletedTask;
+    }
+}
---
+++ ServerlessEndpoint/TriggerMessageHandler.cs
@@ -0,0 +1,16 @@
+﻿using System.Threading.Tasks;
+using Messages;
+using NServiceBus;
+using NServiceBus.Logging;
+
+public class TriggerMessageHandler : IHandleMessages<TriggerMessage>
+{
+    static readonly ILog Log = LogManager.GetLogger<TriggerMessageHandler>();
+
+    public async Task Handle(TriggerMessage message, IMessageHandlerContext context)
+    {
+        Log.Info($"Handling {nameof(TriggerMessage)} in ServerlessEndpoint.");
+        await context.Send(new ResponseMessage()); // use routing config
+        await context.Reply(new ReplyMessage()); // use replyto address
+    }
+}
